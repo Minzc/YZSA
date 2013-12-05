e0c4e3aa242829a6a3871e08c5d895f19c56e243
Minzc <congzicun@gmail.com>
2013-12-05 11:06:42 +0800

    整理feature，按照tfidf选择

--- scripts/tools/slct_f.py
+++ scripts/tools/slct_f.py
@@ -11,7 +11,7 @@ def load_idf(file_path):
     idf_dic = {}
     for ln in lns:
         kw, score = ln.split(' ')
-        idf_dic[kw] = score
+        idf_dic[kw] = float(score)
     return idf_dic
 
 
@@ -20,26 +20,31 @@ def load_tf(file_path):
     tf_dic = {}
     for ln in lns:
         kw, score = ln.split(' ')
-        tf_dic[kw] = score
+        tf_dic[kw] = float(score)
     return tf_dic
 
 
 def cal_tfidf(tf_dic, idf_dic):
     tfidf = {}
-    for kw, score in tf_dic.item():
-        tfidf[kw] = score * idf_dic.get(kw, 0)
+    for kw, score in tf_dic.items():
+        tfidf[kw] = float(score) * idf_dic.get(kw, 0.0)
 
-    std_tfidf = sorted(tfidf.iteritems(), key=operator.itemgetter(1))
+    std_tfidf = sorted(tfidf.iteritems(), key=operator.itemgetter(1), reverse=True)
     return std_tfidf[:10000]
 
 
-def output_tfidf(tfidf):
+def output_tfidf(tfidf, filepath):
+    f_writer = open(filepath, 'w')
     for kw, score in tfidf:
-        print kw.encode('utf-8'), score
+        if len(kw) == 1 or kw.encode('utf-8').isalpha():
+            print kw.encode('utf-8')
+            continue
+
+        f_writer.write(kw.encode('utf-8') + '\t' + str(score) + '\n')
 
 
 if __name__ == '__main__':
     idf_dic = load_idf(sys.argv[1])
     tf_dic = load_tf(sys.argv[2])
     std_tfidf = cal_tfidf(tf_dic, idf_dic)
-    output_tfidf(std_tfidf)
+    output_tfidf(std_tfidf, sys.argv[3])
